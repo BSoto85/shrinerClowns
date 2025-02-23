f74b9dc38e1f46b89be800af76b3d470f2a7f3eb
brenda-soto <bsoto@pursuit.org>
2025-02-23 10:54:22 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function App() {
+  return (
+    <>
+      <FontAwesomeIcon icon={faGithub} />
+    </>
+  );
+}
+
+export default App;
